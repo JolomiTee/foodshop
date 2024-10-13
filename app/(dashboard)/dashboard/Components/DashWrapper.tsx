3bcc9f72d5d907afd9bacd3e837b9f9789e22f8c
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-10-13 20:32:51 +0100

    - code syntax formatting

--- app/(dashboard)/dashboard/Components/DashWrapper.tsx
+++ app/(dashboard)/dashboard/Components/DashWrapper.tsx
@@ -5,28 +5,28 @@ import DashHeader from "./DashHeader";
 import DashSideBar from "./DashSideBar";
 
 type DashWrapperProps = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 };
 
 const DashWrapper = ({ children }: DashWrapperProps) => {
-  const [show, setShow] = useState(false);
-  const showSideBar = () => {
-    setShow(!show);
-  };
-  return (
-    <div className="min-h-screen bg-slate-200 ">
-      <DashSideBar show={show} showSideBar={showSideBar} />
+	const [show, setShow] = useState(false);
+	const showSideBar = () => {
+		setShow(!show);
+	};
+	return (
+		<div className="min-h-screen bg-slate-200 ">
+			<DashSideBar show={show} showSideBar={showSideBar} />
 
-      <section
-        className={`relative  ml-[6.5rem]  transition-all duration-1000 ease-out    ${
-          show && " md:ml-[10rem]"
-        }`}
-      >
-        <DashHeader />
-        {children}
-      </section>
-    </div>
-  );
+			<section
+				className={`relative  ml-[6.5rem]  transition-all duration-1000 ease-out    ${
+					show && " md:ml-[10rem]"
+				}`}
+			>
+				<DashHeader />
+				{children}
+			</section>
+		</div>
+	);
 };
 
 export default DashWrapper;
